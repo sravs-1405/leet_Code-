551b79492a8150e82b63332cc9f3611c2b32f693
SRAVANI KOTA <sravanikota.59@gmail.com>
2026-06-16 13:44:19 +0530

    Time: 1 ms (94.37%) | Memory: 20.4 MB (20.68%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -4,10 +4,10 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         m, n = len(matrix), len(matrix[0])
-        col0 = 1
+        col = 1
         for r in range(m):
             if matrix[r][0] == 0:
-                col0 = 0
+                col = 0
             for c in range(1, n):
                 if matrix[r][c] == 0:
                     matrix[r][0] = 0
@@ -16,5 +16,5 @@ class Solution:
             for c in range(n - 1, 0, -1):
                 if matrix[r][0] == 0 or matrix[0][c] == 0:
                     matrix[r][c] = 0
-            if col0 == 0:
+            if col == 0:
                 matrix[r][0] = 0
